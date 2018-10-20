40998103ecd8534321de1f65007dc5838a7ab1a2
Resse Christophe <christophe.resse@gmail.com>
2018-10-20 12:10:57 +0200

    Setup hemajoo-incubation-jme project.

---
+++ hemajoo-incubation-jme/src/main/java/org/hemajoo/incubation/jme/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains artifacts for the Hemajoo Incubation Software jMonkey Engine project.
+ */
+package org.hemajoo.incubation.jme;
---
+++ hemajoo-incubation-jme/src/test/java/org/hemajoo/incubation/jme/test/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains test artifacts for the Hemajoo Incubation Software jMonkey Engine project.
+ */
+package org.hemajoo.incubation.jme.test;
